dbe3b425cec94040946a54c7b7623b0277176210
jinzhehao <jinzhehao@souche.com>
2024-05-15 09:58:13 +0800

    chore: update dep version

--- src/shared/error.ts
+++ src/shared/error.ts
@@ -1,8 +1,13 @@
+// 添加类型注解
 export default class ExtendableError extends Error {
+  /**
+   * 构造函数，接收一个可选的错误消息
+   * @param message 错误消息，默认为空字符串
+   */
   public constructor(message = "") {
     super(message);
 
-    // extending Error is weird and does not propagate `message`
+    // 修复在继承Error时可能出现的message属性丢失问题
     Object.defineProperty(this, "message", {
       configurable: true,
       enumerable: false,
@@ -10,6 +15,7 @@ export default class ExtendableError extends Error {
       writable: true,
     });
 
+    // 设置错误类的名称
     Object.defineProperty(this, "name", {
       configurable: true,
       enumerable: false,
@@ -17,16 +23,17 @@ export default class ExtendableError extends Error {
       writable: true,
     });
 
-    if (Error.hasOwnProperty("captureStackTrace")) {
+    // 如果Error类有captureStackTrace方法，使用它来设置堆栈跟踪
+    if (typeof Error.captureStackTrace === "function") {
       Error.captureStackTrace(this, this.constructor);
-      return;
+    } else {
+      // 如果没有captureStackTrace，使用new Error(message)来获取堆栈跟踪
+      Object.defineProperty(this, "stack", {
+        configurable: true,
+        enumerable: false,
+        value: new Error(message).stack,
+        writable: true,
+      });
     }
-
-    Object.defineProperty(this, "stack", {
-      configurable: true,
-      enumerable: false,
-      value: new Error(message).stack,
-      writable: true,
-    });
   }
 }
