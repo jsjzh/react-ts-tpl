1484241b475019a5677cfe6dab9d5d5f4bd2d667
jinzhehao <jinzhehao@souche.com>
2024-05-22 17:59:48 +0800

    chore: step

--- src/views/_layouts/Root/index.tsx
+++ src/views/_layouts/Root/index.tsx
@@ -9,6 +9,28 @@ const LayoutRoot: React.FC = () => {
   console.log("location", location);
   console.log("params", params);
 
+  // const history = createBrowserHistory();
+  // history.listen(({ location, action }) => {
+  //   // this is called whenever new locations come in
+  //   // the action is POP, PUSH, or REPLACE
+  // });
+
+  let locations = {
+    pathname: "/bbq/pig-pickins",
+    search: "?campaign=instagram&campaign=instagram2&popular=true",
+    hash: "",
+    state: null,
+    key: "aefz24ie",
+  };
+
+  const query = new URLSearchParams(locations.search);
+  console.log(query);
+  console.log(query.getAll("campaign"));
+
+  // https://reactrouter.com/en/main/start/tutorial#tutorial
+  // https://reactrouter.com/en/main/route/route#children
+  // https://reactrouter.com/en/main/start/concepts#match-params
+
   return <Outlet />;
 };
 
