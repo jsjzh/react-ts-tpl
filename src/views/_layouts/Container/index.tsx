3a3802dba8a46778ecc6aed9cd39668b65c5b823
jinzhehao <jinzhehao@souche.com>
2024-05-23 11:47:18 +0800

    feat: layout 补全

--- src/views/_layouts/Container/index.tsx
+++ src/views/_layouts/Container/index.tsx
@@ -1,8 +1,18 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import styled from "styled-components";
 
-import { Menu } from "antd";
+import { Menu, MenuProps } from "antd";
+import {
+  LaptopOutlined,
+  NotificationOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+
+const styles: { [k: string]: string | number } = {
+  headerHeight: "4rem",
+  siderWidth: "16rem",
+};
 
 const Container = styled.div`
   display: flex;
@@ -23,12 +33,10 @@ const LayoutContainer: React.FC = () => {
 export default LayoutContainer;
 
 const HeaderContainer = styled.div`
-  height: 3.5rem;
-  line-height: 3.5rem;
-  text-align: right;
-  padding-left: 40px;
-  padding-right: 40px;
-  border-bottom: 1px solid rgba(5, 5, 5, 0.06);
+  height: ${styles.headerHeight};
+  line-height: ${styles.headerHeight};
+  padding-left: 2rem;
+  padding-right: 2rem;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.16);
   position: relative;
   z-index: 999;
@@ -39,23 +47,48 @@ const Header: React.FC = () => {
 };
 
 const SiderContainer = styled.div`
-  width: 14rem;
-  height: calc(100vh - 3.5rem);
+  width: ${styles.siderWidth};
+  height: calc(100vh - ${styles.headerHeight});
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.16);
   overflow: auto;
 `;
 
+const items: MenuProps["items"] = [
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+].map((icon, index) => {
+  const key = String(index + 1);
+
+  return {
+    key: `sub${key}`,
+    icon: React.createElement(icon),
+    label: `subnav ${key}`,
+
+    children: new Array(3).fill(null).map((_, j) => {
+      const subKey = index * 4 + j + 1;
+      return {
+        key: subKey,
+        label: `option${subKey}`,
+      };
+    }),
+  };
+});
+
 const Sider: React.FC = () => {
+  const location = useLocation();
+
   return (
     <SiderContainer>
       <Menu
         mode="inline"
         style={{ height: "100%" }}
-        // TODO 这里感觉有些问题，应该是用 pathtree 向上寻址来完成
+        // TODO
+        // 这里感觉有些问题，应该是用 pathtree 向上寻址来完成
         // 但是因为统一的路径命名规范，所以用 split 来分割也行
         defaultOpenKeys={[`/${location.pathname.split("/")[1]}`]}
         selectedKeys={[location.pathname]}
-        items={[]}
+        items={items}
       />
     </SiderContainer>
   );
@@ -64,7 +97,7 @@ const Sider: React.FC = () => {
 const ContentContainer = styled.div`
   flex: 1;
   overflow: auto;
-  height: calc(100vh - 3.5rem);
+  height: calc(100vh - ${styles.headerHeight});
 `;
 
 const Content: React.FC = () => {
