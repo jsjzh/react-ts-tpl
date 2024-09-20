3ff9108295359a3bcce6c8908899f9c8457411f7
jsjzh <kimimi_king@163.com>
2024-09-20 19:08:32 +0800

    style: step

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,15 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 
+import {
+  StyleProvider,
+  legacyLogicalPropertiesTransformer,
+} from "@ant-design/cssinjs";
 import { ConfigProvider } from "antd";
 import zhCN from "antd/es/locale/zh_CN";
 
 import "antd/dist/reset.css";
 import "./index.less";
-
-import {
-  StyleProvider,
-  legacyLogicalPropertiesTransformer,
-} from "@ant-design/cssinjs";
 
 import dayjs from "dayjs";
 import "dayjs/locale/zh-cn";
